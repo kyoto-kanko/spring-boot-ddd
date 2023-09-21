218305f1e6ccad457d3f59f03ca6d764140ab1c5
kyoto-kanko <kyoto.kanko.biz@gmail.com>
2023-09-22 01:57:26 +0900

    update Userのデモデータ追加

---
+++ app/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO users (id, name, age) VALUES ('1', 'Alice', 30);
+INSERT INTO users (id, name, age) VALUES ('2', 'Bob', 40);
+INSERT INTO users (id, name, age) VALUES ('3', 'Charlie', 50);
